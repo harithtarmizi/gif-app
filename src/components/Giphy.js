fc235175ba09566f4b5d77aab6f9cddd56fcb037
harithtech <harith.trmz@gmail.com>
2022-04-02 23:29:21 +0800

    1. call api gif trending

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 
 function Giphy() {
+
+	const [giphy, setGiphy] = useState([])
+
+	let getGiphy = async () => {
+		try{
+			const results = await axios('https://api.giphy.com/v1/gifs/trending', {
+				params: {
+					api_key: "irmPwk72VjmepnE38NDbkGKF9HutMRR0",
+					limit: 25,
+				}
+			})
+			setGiphy(results)
+		}catch(e) {
+			console.log(e)
+		}
+	}
+
 	return(
 		<div>
 			<h1>This is the Giphy Component</h1>
