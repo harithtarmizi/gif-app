25b55ae52d32ad4d03108259663c68e0cbf7b2a6
harithtech <harith.trmz@gmail.com>
2022-04-03 17:19:25 +0800

    1. implement search gif API

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -6,12 +6,14 @@ import { useState, useEffect } from 'react';
 function Giphy() {
 
 	const [giphy, setGiphy] = useState([])
+	const [search, setSearch] = useState("")
 
 	let getGiphy = async () => {
 		try{
 			const results = await axios('https://api.giphy.com/v1/gifs/trending', {
 				params: {
 					api_key: "irmPwk72VjmepnE38NDbkGKF9HutMRR0",
+					q: search,
 					limit: 25,
 				}
 			})
@@ -34,13 +36,33 @@ function Giphy() {
 		)
 	})
 
+	let onChangeHandler = (e) => {
+		setSearch(e.target.value)
+	}
+
+	let onSubmitHandler = async (e) => {
+		e.preventDefault()
+		try{
+			const searchResult = await axios('https://api.giphy.com/v1/gifs/search', {
+				params: {
+					api_key: "irmPwk72VjmepnE38NDbkGKF9HutMRR0",
+					q: search,
+					limit: 25,
+				}
+			})
+			setGiphy(searchResult.data.data)
+		}catch(e) {
+			console.log(e)
+		}
+	}
+
 	return(
-		<div>
-			<h1>This is the Giphy Component</h1>
-			<br />
-			<div className='gif-container'>
-				{showGiphy}	
-			</div>
+		<div className='gif-container'>
+			<form className='gif-form' onSubmit={onSubmitHandler}>
+				<input type="text"className='search-box' onChange={onChangeHandler}/>
+				<button>Hit Me</button>
+			</form>
+			{showGiphy}	
 		</div>
 	)
 }
